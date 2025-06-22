3d94502e09b13ae2db9ce369e732c8d781d22846
DocW <docwhomc@gmail.com>
2025-06-22 15:55:00 -0400

    Initial commit

---
+++ LostSkiesDataDump/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+namespace LostSkiesDataDump;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Lost Skies Data Dump
+
+A mod for outputting data from the game Lost Skies.
+
+This mod is primarily intended to aid in maintaining up to date game
+data in the [Lost Skies Wiki](https://lostskies.wiki.gg/).
+
+## Disclaimer
+
+This is not an official Lost Skies product.  This is not approved by or
+associated with Bossa Games or Humble Bundle.
