20426f0426b6225244bc75a5aae1cba3839a8af0
DocW <docwhomc@gmail.com>
2025-06-22 17:38:15 -0400

    Add copyright notice to Plugin.cs

--- LostSkiesDataDump/Plugin.cs
+++ LostSkiesDataDump/Plugin.cs
@@ -1,4 +1,22 @@
-﻿using BepInEx;
+﻿/*
+  LostSkiesDataDump: A mod for outputting data from the game Lost Skies.
+  Copyright (C) 2025  DocW
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using BepInEx;
 using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 
