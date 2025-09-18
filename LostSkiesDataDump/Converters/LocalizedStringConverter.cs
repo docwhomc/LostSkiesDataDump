6bedb588f9c680343eb44e66c6d1d046182bc5e9
DocW <docwhomc@gmail.com>
2025-09-18 17:13:13 -0400

    Reduce logging level of LocalizedString w/o table
    Reduce logging level for LocalizedString that does not specify a table
    collection from warning to debug.  Also include a comment in JSON output
    files noting the issue.

--- LostSkiesDataDump/Converters/LocalizedStringConverter.cs
+++ LostSkiesDataDump/Converters/LocalizedStringConverter.cs
@@ -49,7 +49,11 @@ public class LocalizedStringConverter<T> : BaseConverter<T> where T : LocalizedS
             value.LocaleOverride = initialLocale;
         }
         else
-            Plugin.Log.LogWarning("Unable to serialize LocalizedString that does not specify a table collection");
+        {
+            var message = "Unable to serialize LocalizedString that does not specify a table collection";
+            writer.WriteCommentValue(message);
+            Plugin.Log.LogDebug(message);
+        }
     }
 
     public static bool HasTableReference(LocalizedString localizedString) => localizedString.TableReference is not null && (localizedString.TableReference.TableCollectionName is not null || localizedString.TableReference.TableCollectionNameGuid != Il2CppSystem.Guid.Empty);
