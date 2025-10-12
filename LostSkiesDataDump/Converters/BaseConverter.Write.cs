docwhomc/LostSkiesDataDump

commit 3038c18808d29c97ae0793139925cd2575c838ce

Add methods w/automatic name arg to BaseConverter
Add versions of the `WriteArray(...)` and `WriteProperty(...)` methods
of `BaseConverter` that automatically obtain values for the `name`
argument via `CallerArgumentExpression`.  Mark the versions of
`WriteArray(...)` and `WriteProperty(...)` that **require** manual
specification of the `name` argument as obsolete.

LostSkiesDataDump/Converters/BaseConverter.Write.cs
@@ -18,6 +18,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -60,7 +61,13 @@ public abstract partial class BaseConverter<T> : JsonConverter<T>
         return JsonSerializer.Serialize;
     }
 
+    [ObsoleteAttribute($"{nameof(WriteArray)}<V>(Utf8JsonWriter, string, IEnumerable<V>, JsonSerializerOptions) is obsolete, use {nameof(WriteArray)}<V>(Utf8JsonWriter, IEnumerable<V>, JsonSerializerOptions, [string name = null]) instead.")]
     public static void WriteArray<V>(Utf8JsonWriter writer, string name, IEnumerable<V> value, JsonSerializerOptions options)
+    {
+        WriteArray(writer, value, options, name);
+    }
+
+    public static void WriteArray<V>(Utf8JsonWriter writer, IEnumerable<V> value, JsonSerializerOptions options, [CallerArgumentExpression(nameof(value))] string name = null)
     {
         var serializer = GetSerializer<V>(options);
         try
@@ -79,7 +86,13 @@ public abstract partial class BaseConverter<T> : JsonConverter<T>
         }
     }
 
+    [ObsoleteAttribute($"{nameof(WriteProperty)}<V>(Utf8JsonWriter, string, IEnumerable<V>, JsonSerializerOptions) is obsolete, use {nameof(WriteArray)}<V>(Utf8JsonWriter, IEnumerable<V>, JsonSerializerOptions, [string name = null]) instead.")]
     public static void WriteProperty<V>(Utf8JsonWriter writer, string name, V value, JsonSerializerOptions options)
+    {
+        WriteProperty(writer, value, options, name);
+    }
+
+    public static void WriteProperty<V>(Utf8JsonWriter writer, V value, JsonSerializerOptions options, [CallerArgumentExpression(nameof(value))] string name = null)
     {
         var serializer = GetSerializer<V>(options);
         try

commit 7aab8d5b6497645deb371d920a65f77335e1ade5

Patch automatic naming bug introduced in 3038c18
Patch automatic naming bug introduced in commit 3038c18 by stripping
`value.` from the beginning of names/keys.  Also, strip `.Select(...)`
and `.ToSystemEnumerable()` from the end of names/keys to eliminate need
to manually specify names/keys when such methods are called.

Since stripping of unnecessary parts of names/keys is done at runtime,
cache the results.

LostSkiesDataDump/Converters/BaseConverter.Write.cs
@@ -21,14 +21,41 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
 using icg = Il2CppSystem.Collections.Generic;
 
 namespace LostSkiesDataDump.Converters;
 
 public abstract partial class BaseConverter<T> : JsonConverter<T>
 {
+    private static readonly Regex s_nameCleaner = new(
+        "^(?:value\\.)?(.*?)(?:\\.(?:Select\\(.*\\)|ToSystemEnumerable\\(\\)))?$"
+    );
+    private static readonly Dictionary<string, string> s_nameCleanerCache = [];
+
+    public static void ClearNameCleanerCache()
+    {
+        s_nameCleanerCache.Clear();
+    }
+
+    public static string CleanName(string name)
+    {
+        if (s_nameCleanerCache.TryGetValue(name, out string cached))
+            return cached;
+        var match = s_nameCleaner.Match(name);
+        var cacheKey = name;
+        if (match.Success)
+            name = match.Groups[1].Value;
+        else
+            Plugin.Log.LogWarning($"name `{name}` didn't match pattern");
+        Plugin.Log.LogDebug($"Adding name cleaner cache entry: `{cacheKey}` -> `{name}`");
+        s_nameCleanerCache.Add(cacheKey, name);
+        return name;
+    }
+
     public static JsonEncodedText EncodeName(string name, JsonSerializerOptions options)
     {
+        name = CleanName(name);
         return JsonEncodedText.Encode(options.PropertyNamingPolicy?.ConvertName(name) ?? name);
     }
 